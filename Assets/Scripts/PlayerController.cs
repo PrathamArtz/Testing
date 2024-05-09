3e13d50e24889e360097fbfbb335639d1ece4834
Prathamesh-Pulkanthwar <Pulkanthwarprathamesh2@gmail.com>
2024-05-09 18:40:50 +0530

    Revert "Rigidbody_Player_Movement"
    This reverts commit 066ce8dbc0944cf5e001d1f9a19efd189eb1426b.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,33 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //[SerializeField] private float playerSpeed = 30.0f;
-
-    [SerializeField]
-    private Rigidbody playerRB;  // geting rigidbody component for movement.
-    
-
-    // Add Rigidbody to the Player.
-
-    private void FixedUpdate()
+    // Start is called before the first frame update
+    void Start()
     {
         
-        if (Input.GetKey("a"))
-        {
-            playerRB.AddForce(-10, 0, 0 * Time.deltaTime);
-        }
-         else if(Input.GetKey("d"))
-        {
-            playerRB.AddForce(10, 0, 0 * Time.deltaTime);
-        }
-        else if(Input.GetKey("w"))
-        {
-            playerRB.AddForce(0, 0, 1000 * Time.deltaTime);
-        }
-        else if (Input.GetKey("s"))
-        {
-            playerRB.AddForce(0, 0, -1000 * Time.deltaTime);
-        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
