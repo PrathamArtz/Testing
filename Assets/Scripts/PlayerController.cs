b4beceb6bd3b3a12f78f4a87ebc91f6a4b0ac629
Prathamesh-Pulkanthwar <Pulkanthwarprathamesh2@gmail.com>
2024-05-09 22:11:42 +0530

    Charecter_Controller ( BY Input Manager )

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField]
+    private float _playerSpeed = 3.0f;
+    
     void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+         transform.Translate(Vector3.right * horizontalInput * _playerSpeed * Time.deltaTime);
+         transform.Translate(Vector3.forward * verticalInput * _playerSpeed * Time.deltaTime);
         
+       
     }
 }
